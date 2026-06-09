b9f0df20130b076ef88b92adba697031c7df9a9b
Hector Marquez <hectordmv21@gmail.com>
2026-06-09 11:49:18 -0400

    feat: conectar google sheets

--- src/lib/configNube.ts
+++ src/lib/configNube.ts
@@ -5,7 +5,7 @@
  *
  * Cómo obtenerla: ver apps-script/INSTRUCCIONES.md
  */
-export const URL_NUBE = '';
+export const URL_NUBE = 'https://script.google.com/macros/s/AKfycbzkysCQhQtJWUNudA-HEJBzwApBPhZEfntzjWFjAVde8d6rkio5YcwP4QFW9h-8Eecu/exec';
 
 export function nubeActiva(): boolean {
   return URL_NUBE.trim().length > 0;
